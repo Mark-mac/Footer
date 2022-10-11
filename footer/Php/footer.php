5b98b860315ade4c33e124aca01236596547d873
Mark E Mutugi <79484425+Mark-mac@users.noreply.github.com>
2022-10-11 05:34:37 -0700

    Add files via upload

---
+++ footer/Php/footer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../Css/footer.css">
+    <link rel="icon" 
+      type="image/png" 
+      href="../Images/logo.png">
+    <title>Footer</title>
+</head>
+<body>
+<div class="main-footer">
+<div class="footer-one">
+  <div class="logo-title">
+   <div class="footer-logo">
+        <img src="../Images/logo.png">
+    </div>
+    <div class="footer-title">
+     <p class="school-title">DEB Kavutiri mixed Secondary School.</p>
+     <p class="pobox">P.O BOX 1897-60100.</p>
+     <p class="poboxx">Nairobi.</p>
+    </div>
+  </div>
+  <hr>
+    <div class="mo-mi-vi">
+    <p class="motto">
+     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Commodi minus officia consequatur, dolorem assumenda ducimus expedita optio iusto. Quaerat, pariatur?
+     </p>
+     <p class="mission">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit saepe, repellendus alias vero sunt obcaecati sed, quas a beatae accusamus numquam impedit dicta aut necessitatibus, vitae nostrum maiores. Necessitatibus, temporibus.</p>
+     <p class="vision">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia fuga ad repellat quaerat dolorum delectus vel, voluptatum, suscipit accusamus quibusdam ut asperiores voluptate laboriosam esse in, molestiae est obcaecati unde!</p>
+    </div>
+    <!-- <div class="footer-nav">
+     <ul>
+    <li><a href="about.php">ABOUT</a></li>
+    <li><a href="contacts.php">CONTACTS</a></li>
+    <li><a href="help.php">HELP</a></li>
+    <li><a href="settings.php">SETTINGS</a></li>
+    <li><a href="signout.php">SIGN OUT</a></li>
+     </ul>
+    </div> -->
+    </div>
+    <hr>
+    <div class="footer-two">
+        <div><img src="../Images/logo.png"></div>
+        <div><img src="../Images/logo.png"></div>
+        <div><img src="../Images/logo.png"></div>
+        <div><img src="../Images/logo.png"></div>
+        <div><img src="../Images/logo.png"></div>
+    </div>
+    </div>
+</body>
+</html>
